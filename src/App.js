ac6d56148902d71c96fe7e19939fbeb113a7ca32
GentilleauF <francois.gentilleau@gmail.com>
2023-12-20 09:58:33 +0100

    Starter avec bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { createData, readData, deleteData, updateData } from './Firebase/firebaseCRUD'
+import React, { useState, useEffect } from "react";
+import {
+  createData,
+  readData,
+  deleteData,
+  updateData,
+} from "./Firebase/firebaseCRUD";
 
 function App() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
@@ -18,47 +23,69 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     createData(title, author);
-    setTitle('');
-    setAuthor('');
-    readData().then(data => setArticles(data));
+    setTitle("");
+    setAuthor("");
+    readData().then((data) => setArticles(data));
   };
 
   const handleDelete = (id) => {
     deleteData(id);
-    readData().then(data => setArticles(data));
+    readData().then((data) => setArticles(data));
   };
 
   const handleUpdate = (id, newData) => {
     updateData(id, newData);
-    readData().then(data => setArticles(data));
+    readData().then((data) => setArticles(data));
   };
 
   return (
     <div>
-      <h1>Article Management</h1>
+    <div className="container text-center bg-light p-1">
+    <div>
       <form onSubmit={handleSubmit}>
-        <label>
-          Title:
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-        </label>
-        <label>
-          Author:
-          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </label>
-        <button type="submit">Add Article</button>
+        <h1>Ajouter vos livres</h1>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Titre du livre:
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Description:
+          </label>
+          <textarea
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="form-control"
+            rows="3"
+          ></textarea>
+        </div>
+        <div className="mb-3">
+          <button type="submit" className="btn btn-outline-primary">
+            Ajouter un Article
+          </button>
+        </div>
       </form>
-      <ul>
-        {articles.map(article => (
-          <li key={article.id}>
-            {article.title} - {article.author}
-            <button onClick={() => handleDelete(article.id)}>Delete</button>
-            <button onClick={() => handleUpdate(article.id, { title: 'Updated Title', author: 'Updated Author' })}>Update</button>
-          </li>
-        ))}
-      </ul>
     </div>
+  </div>
+  <ul>
+    {articles.map(article => (
+      <li key={article.id} >
+        {article.title} - {article.author}
+        <button onClick={() => handleDelete(article.id)} >Supprimer</button>
+        <button onClick={() => handleUpdate(article.id, {title: 'Updated Title', author: 'Updated Author'})} >Update</button>
+      </li>
+      ))
+      }
+  </ul>
+</div>
   );
 }
 
 export default App;
-
--- src/Components/AddArticle.js
+++ src/Components/AddArticle.js
@@ -2,58 +2,48 @@
 // import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
 // const AddArticle = () => {
-//   const [data, setData] = [];
-
-//   function addNewBook() {}
-
-//   //READ
-//   getDocs(colRef)
-//     .then((snapshot) => {
-//       let books = [];
-//       snapshot.docs.forEach((doc) => {
-//         books.push({ ...doc.data(), id: doc.id });
-//       });
-//       console.log(books[0].title);
-//     })
-//     .then(setData([...books]));
-
 //   return (
 //     <div className="container text-center bg-light p-1">
 //       <div>
-//         <form>
-//           <h1>Ajouter un Article</h1>
+//         <form onSubmit={handleSubmit}>
+//           <h1>Ajouter vos livres</h1>
 //           <div className="mb-3">
 //             <label htmlFor="exampleFormControlInput1" className="form-label">
-//               Titre de l'article
+//               Titre du livre:
 //             </label>
-//             <input type="text" className="form-control" />
+//             <input
+//               type="text"
+//               value={title}
+//               onChange={(e) => setTitle(e.target.value)}
+//               className="form-control"
+//             />
 //           </div>
 //           <div className="mb-3">
 //             <label htmlFor="exampleFormControlTextarea1" className="form-label">
-//               Description article
+//               Description:
 //             </label>
-//             <textarea className="form-control" rows="3"></textarea>
+//             <textarea
+//               value={author}
+//               onChange={(e) => setAuthor(e.target.value)}
+//               className="form-control"
+//               rows="3"
+//             ></textarea>
 //           </div>
 //           <div className="mb-3">
-//             <button
-//               onClick={addNewBook}
-//               type="button"
-//               className="btn btn-outline-primary"
-//             >
+//             <button type="submit" className="btn btn-outline-primary">
 //               Ajouter un Article
 //             </button>
 //           </div>
 //         </form>
-//       </div>
-//       <div>
 //         <ul>
-//           {console.log(data)}
-
-//           {/* {books.forEach((item, index) => (
-//           // Attention : forEach ne retourne pas de tableau
-//           // Vous devriez utiliser map pour créer un tableau de composants React
-//           <li key={index}>{item}</li>
-//         ))} */}
+//             {article.map(article => (
+//                 <li key={article.id} >
+//                     {article.title} - {article.author}
+//                     <button onClick={() => handleDelete(article.id)} >Supprimer</button>
+//                     <button onClick={() => handleUpdate(article.id, {title: 'Updated Title', author: 'Updated Author'})} >Update</button>
+//                 </li>
+//             ))
+//             }
 //         </ul>
 //       </div>
 //     </div>
